d4e4b2d8a395f72ce8846cad66ea4547c8f1666c
unknown <demateos11@gmail.com>
2021-06-30 02:48:15 +0200

    repos mvp

---
+++ script.js
@@ -0,0 +1,80 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'correct Number !';
+// document.querySelector('.score').textContent = '10';
+// document.querySelector('.number').textContent = '13';
+// document.querySelector('.guess').value = '23';
+// console.log(document.querySelector('.guess').value);
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+// console.log(secretNumber);
+
+
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+
+ 
+
+
+// when there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = ' fo ko !';
+
+// when player win 
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = ' an da in tu !';
+
+  document.querySelector('body').style.backgroundColor = '#60b347';
+  document.querySelector('.number').style.width = '30rem';
+  document.querySelector('.number').textContent = secretNumber;
+
+// implement highscore
+if(score > highscore){
+  highscore = score;
+  document.querySelector('.highscore').textContent = highscore;
+}
+ 
+// when guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'ho bagadi !';
+      score--; // score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'an xose';
+      score--; // score = score - 1;
+      document.querySelector('.score').textContent = 0;
+    }
+  
+  // when guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Ho kahayi !';
+      score--; // score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'an xose';
+      score--; // score = score - 1;
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+// reinitialisation
+document.querySelector('.again').addEventListener('click', function(){
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'sangen joppe ....';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+} );
