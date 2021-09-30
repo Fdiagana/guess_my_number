b79f6c7e16c7d4627191bb69ac6e039058b14bda
unknown <demateos11@gmail.com>
2021-09-30 18:05:48 +0200

    english

--- script.js
+++ script.js
@@ -19,17 +19,13 @@ let highscore = 0;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
-
- 
-
-
 // when there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = ' fo ko !';
+    document.querySelector('.message').textContent = ' No number !';
 
 // when player win 
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = ' an da in tu !';
+    document.querySelector('.message').textContent = ' you win !';
 
   document.querySelector('body').style.backgroundColor = '#60b347';
   document.querySelector('.number').style.width = '30rem';
@@ -44,11 +40,11 @@ if(score > highscore){
 // when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'ho bagadi !';
+      document.querySelector('.message').textContent = 'too high !';
       score--; // score = score - 1;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'an xose';
+      document.querySelector('.message').textContent = 'you loose !';
       document.querySelector('body').style.backgroundColor = '#FF0000'; 
       score--; // score = score - 1;
       document.querySelector('.score').textContent = 0;
@@ -57,11 +53,11 @@ if(score > highscore){
   // when guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Ho kahayi !';
+      document.querySelector('.message').textContent = 'too low !';
       score--; // score = score - 1;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'an xose';
+      document.querySelector('.message').textContent = 'you loose';
       document.querySelector('body').style.backgroundColor = '#FF0000';
       score--; // score = score - 1;
       document.querySelector('.score').textContent = 0;
